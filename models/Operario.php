d3497be4755f4ddc87b9d95007a829ed686437a5
Lou-Luna <luisxluna@gmail.com>
2026-08-04 18:33:54 -0500

    Se crea el objeto operario

---
+++ models/Operario.php
@@ -0,0 +1,54 @@
+<?php
+
+/*
+|
+| Clase operario
+|
+| Esta clase contiene los atributos del operario
+| 
+|
+*/
+
+class Operario{
+
+    private $idOperario;
+    private $nombre;
+    private $rol;
+
+    //Constructor
+    public function __construct($idOperario = null, $nombre = null, $rol = null) {
+        $this->idOperario = $idOperario;
+        $this->nombre = $nombre;
+        $this->rol = $rol;
+    }
+
+    public function getIdOperario(){
+        return $this->idOperario;
+    }
+
+    public function setIdOperario($idOperario){
+        $this->idOperario = $idOperario;
+    }
+
+    public function getNombre(){
+        return $this->nombre;
+    }
+
+    public function setNombre($nombre){
+        $this->nombre = $nombre;
+    }
+
+    public function setRol(){
+        return $this->rol;
+    }
+
+    public function getRol($rol){
+        $this->rol = $rol;
+    }
+
+    //Método opcional para imprimir el objeto como texto
+    public function __toString() {
+        return "Operario [ID: {$this->idOperario}, Nombre: {$this->nombre}, Rol: {$this->rol}]";
+    }
+
+}
